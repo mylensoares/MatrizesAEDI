7517bf1a8e1f0a53b4cff32bc25d8de55d4355ff
mylenteles <mylenateles@hotmail.com>
2022-04-30 15:01:23 -0300

    Funções

--- funcoes.py
+++ funcoes.py
@@ -14,8 +14,27 @@ def matriz_transposta(matriz):
             print(f"{matriz[j][i]:2d}", end=" ")
         print()
 
+# Soma diagonal Principal de uma matriz 4x4
+def soma_DiagonalPrincipal(matriz):
+    for i in matriz:
+        pri_elemento = matriz[0][0]
+        seg_elemento = matriz[1][1]
+        ter_elemento = matriz[2][2]
+        qua_elemento = matriz[3][3]
+        soma = pri_elemento + seg_elemento + ter_elemento + qua_elemento
+    return print(f"A soma a digonal a matriz é {soma}")
+
+# Exibindo apenas os números pares da matriz
+def pares_matriz(matriz):
+    cont = 0
+    paresMatriz = []
+    while cont < len(matriz):
+        cont += 1
+        if matriz[cont] % 2 == 0:
+            paresMatriz.append(matriz[cont])
+
 
 matriz = [[9, 7, 4, 2], [10, 13, 18, 21], [33, 5, 7, 90], [23, 31, 51, 60]]
 
-matriz_Original(matriz)
-matriz_transposta(matriz)
+soma_DiagonalPrincipal(matriz)
+pares_matriz(matriz)
