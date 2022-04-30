908a3b00c9836f16f6b2d4dfddc5bac23a0b61b6
mylenteles <mylenateles@hotmail.com>
2022-04-30 13:42:41 -0300

    Menu Principal

---
+++ menu.py
@@ -0,0 +1,11 @@
+# Menu
+def menu():
+    print("""Menu Principal
+1. Matriz Original
+2. Matriz transposta
+3. Mostre a soma da diagonal principal da matriz
+4. Mostre os números pares da matriz
+5. Mostre os números ímpares da matriz
+0. – Sair do sistema
+    """)
+menu()
