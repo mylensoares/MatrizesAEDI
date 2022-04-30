6be8ed3edd753a2c0a7c8e7f7e3fe2c8e171209a
mylenteles <mylenateles@hotmail.com>
2022-04-30 13:56:58 -0300

    Funções

--- funcoes.py
+++ funcoes.py
@@ -7,7 +7,15 @@ def matriz_Original(matriz):
             print(f"{matriz[i][j]:2d}", end=" ")
         print()
 
-# Exibindo a matriz transposta
+# Exibindo a matriz transposta com inversão das variáveis de controle i e j 
+def matriz_transposta(matriz):
+    for i in range(len(matriz)):
+        for j in range((len(matriz[0]))):
+            print(f"{matriz[j][i]:2d}", end=" ")
+        print()
+
+
 matriz = [[9, 7, 4, 2], [10, 13, 18, 21], [33, 5, 7, 90], [23, 31, 51, 60]]
 
 matriz_Original(matriz)
+matriz_transposta(matriz)
